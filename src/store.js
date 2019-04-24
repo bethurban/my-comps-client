d8e17c93ea3af630bc0d5cecd68e4f40a28b67f5
bethurban <beth.urban@gmail.com>
2019-04-24 16:30:40 -0400

    Add redux and redux-thunk. Build searchesReducer.

--- src/store.js
+++ src/store.js
@@ -2,4 +2,17 @@ import {
   createStore,
   applyMiddleware,
   combineReducers
-} from 'redux'
+} from 'redux';
+import thunk from 'redux-thunk';
+
+const searchesReducer  = (state = [], action) => {
+  switch(action.type) {
+    case 'GET_SEARCHES_SUCCESS':
+      return action.searches;
+    default:
+      return state;
+  }
+}
+
+let searches = searchesReducer();
+console.log(search) = []
