bcb6554a8a9e546262d2a31897450c7464294068
bethurban <beth.urban@gmail.com>
2019-04-24 16:58:20 -0400

    Add react-redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
@@ -8,7 +9,9 @@ import store from './store.js';
 console.log(store)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
