212407d41f2381f20286e7a1dc4ed2a6f1660a43
bethurban <beth.urban@gmail.com>
2019-04-25 14:23:35 -0400

    Create Searches container

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css'
 import Homes from '../containers/Homes.js'
+import Searches from '../containers/Searches.js'
 
 const App = () => (
   <div className="App">
+    <Searches />
     <Homes />
   </div>
 )
---
+++ src/containers/Searches.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Searches extends Component {
+  render() {
+    return (
+      <h1>This is where the Zillow search will go</h1>
+    )
+  }
+
+}
+
+export default Searches;
