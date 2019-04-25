a385359ce16c66a9e976c8d7dde7e5f972aa26b3
bethurban <beth.urban@gmail.com>
2019-04-25 10:53:13 -0400

    Convert App.js to component.

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+import Searches from '../containers/Searches.js'
+
+const App = () => (
+  <div className="App">
+    <Searches />
+  </div>
+)
+
+
+export default App;
