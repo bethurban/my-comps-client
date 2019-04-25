ed71d9c99b0795a6210825ee6c4c261c44475a21
bethurban <beth.urban@gmail.com>
2019-04-25 18:57:59 -0400

    Work on CORS access issues with Zillow API

--- src/actions/searches.js
+++ src/actions/searches.js
@@ -18,12 +18,15 @@ const setSearches = searches => {
 // ** Async Actions **
 export const getSearches = () => {
   return dispatch => (
-    fetch('https://crossorigin.me/http://www.zillow.com/webservice/GetDeepComps.htm?zws-id=X1-ZWz1h1ekqqlfrf_70ucn&zpid=52635337&count=5')
-      .then(resp => console.log(resp))
-      // .then(resp => resp.json())
-      // .then(searches => dispatch(setSearches(searches)))
-      // .then(resetSearchForm())
-      .catch(error => console.log(error))
+    fetch('http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz1h1ekqqlfrf_70ucn&state=nj', { mode: 'no-cors' })
+      .then(response => response.text())
+      .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+      .then(data => console.log(data))
+    //   .then(resp => console.log(resp))
+    // //   // .then(resp => resp.json())
+    // //   // .then(searches => dispatch(setSearches(searches)))
+    // //   // .then(resetSearchForm())
+    //   .catch(error => console.log(error))
   )
 }
 //
