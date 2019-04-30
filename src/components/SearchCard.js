eb5a11dd12e4c68be09d047c9641e9b36ed84141
bethurban <beth.urban@gmail.com>
2019-04-30 18:23:28 -0400

    Add class of cardImage to SearchCard's image and add CSS for sizing

--- src/components/SearchCard.js
+++ src/components/SearchCard.js
@@ -5,7 +5,7 @@ const SearchCard = ({ property, image }) => (
     <div className="card">
       <div className="card__face card__face--front">
         <h3>Searched property</h3>
-        <img src={ image } alt={ property[0] } width="90%" height="70%"/>
+        <img src={ image } alt={ property[0] } className="cardImage" />
         <p>{ property[0] }</p>
       </div>
       <div className="card__face card__face--back">
