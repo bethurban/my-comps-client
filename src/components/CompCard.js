f237ae946377abe07d3038821d2d5c176eebfd42
bethurban <beth.urban@gmail.com>
2019-05-01 15:42:30 -0400

    HTML formatting on CompCard and SearchCard:

--- src/components/CompCard.js
+++ src/components/CompCard.js
@@ -8,8 +8,8 @@ const CompCard = ({ comp }) => (
         <p className="compText">Last sold on { comp[6] } for ${ comp[7] }</p>
       </div>
       <div className="card__face card__face--back">
-        <p className="compText">Built in { comp[1] }. { comp[3] } bedrooms, { comp[4] } bathrooms.</p>
-        <p className="compText">Finished interior size: { comp[2] } square feet. Lot size: { comp[5] } square feet.</p>
+        <p className="compText">Last sold on { comp[6] } for ${ comp[7] }</p>
+        <p className="compText">Built in { comp[1] }. { comp[3] } bedrooms, { comp[4] } bathrooms. Finished interior size: { comp[2] } square feet. Lot size: { comp[5] } square feet.</p>
       </div>
     </div>
     <p><a href={ comp[8] } target="_blank" rel="noopener noreferrer">Zillow link</a></p>
--- src/components/SearchCard.js
+++ src/components/SearchCard.js
@@ -6,15 +6,17 @@ const SearchCard = ({ property, image }) => (
       <div className="card__face card__face--front">
         <h3>Searched property</h3>
         <img src={ image } alt={ property[0] } className="cardImage" onError={(e)=>{e.target.onerror = null; e.target.src="https://cdn.dribbble.com/users/440793/screenshots/2744373/dribble.jpg"}} />
-        <p>{ property[0] }</p>
+        <h4>{ property[0] }</h4>
+        <p>Last sold on { property[6] } for ${ property[7] }</p>
         <p><a href={ property[8] } target="_blank" rel="noopener noreferrer">Zillow link</a></p>
       </div>
       <div className="card__face card__face--back">
+        <h3>Searched property</h3>
         <p>Last sold on { property[6] } for ${ property[7] }</p>
-        <p>Year built: { property[1] }</p>
-        <p>Finished interior size: { property[2] } square feet</p>
-        <p> { property[3] } bedrooms, { property[4] } bathrooms</p>
-        <p> Lot size: { property[5] } square feet</p>
+        <p>Built in { property[1] }.</p>
+        <p>{ property[3] } bedrooms, { property[4] } bathrooms.</p>
+        <p>Finished interior size: { property[2] } square feet.</p>
+        <p>Lot size: { property[5] } square feet.</p>
       </div>
     </div>
   </div>
