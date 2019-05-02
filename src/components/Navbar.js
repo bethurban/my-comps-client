66cd3f7116516ffa66702bec54f3d3df699440e0
bethurban <beth.urban@gmail.com>
2019-05-02 13:13:32 -0400

    Add Google login button to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
+import GoogleLogin from 'react-google-login';
 
 const Navbar = () => {
+  const GOOGLE_ID = process.env.REACT_APP_GOOGLE_ID
+
+  const responseGoogle = (response) => {
+      console.log(response);
+    }
   return(
     <nav className="topnav">
       <div className="container">
         <Link to='/' className="logo">My Comps</Link>
         <Link to='/about'>About</Link>
+        <GoogleLogin
+          clientId={GOOGLE_ID}
+          buttonText="Log in with Google"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+        />
       </div>
     </nav>
   )
